4d1d660f7f4a6e498ca02f1a7807264ee312ce0a
Anbiya Popal <anbiya2004@gmail.com>
2021-07-07 13:35:25 +0200

    limit king movement (no castling), doesnt take other pieces into account yet

--- src/pieces/king.py
+++ src/pieces/king.py
@@ -10,8 +10,21 @@ class King(pieces.Piece):
         self.short_castle_allowed = None
         self.long_castle_allowed = None
 
-    def legal_moves(self, current_board):
-        pass
+    def legal_moves(self):
+        legal_squares = []
+
+        legal_squares.append([self.x - 1, self.y -1]) # bottom left (like all following comments, from white's perspective)
+        legal_squares.append([self.x - 1, self.y]) # left
+        legal_squares.append([self.x - 1, self.y + 1]) # top left
+
+        legal_squares.append([self.x, self.y - 1]) # down
+        legal_squares.append([self.x, self.y + 1]) # up
+
+        legal_squares.append([self.x + 1, self.y - 1]) # bottom right
+        legal_squares.append([self.x + 1, self.y]) # right
+        legal_squares.append([self.x + 1, self.y + 1]) # top right
+
+        return legal_squares
 
     def castle_allowed(self, current_board):
         if self.previous_moves == []:
