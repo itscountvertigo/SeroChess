3bde4f2ed5176a83a4e814e222e65b906b0d68a8
Anbiya Popal <anbiya2004@gmail.com>
2021-07-07 12:46:07 +0200

    cleaner import

--- src/main.py
+++ src/main.py
@@ -1,6 +1,1 @@
-from pieces import king
-from pieces import queen
-from pieces import rook
-from pieces import knight
-from pieces import bishop
-from pieces import pawn
+from pieces import king, queen, rook, knight, bishop, pawn
