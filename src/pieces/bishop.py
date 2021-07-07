e87828a56efd9ca520bed0d9167aaf6de026a3fb
Anbiya Popal <anbiya2004@gmail.com>
2021-07-07 13:28:01 +0200

    legal moves calculation for diagonal moving pieces (queen/bishop)

--- src/pieces/bishop.py
+++ src/pieces/bishop.py
@@ -7,8 +7,34 @@ class Bishop(pieces.Piece):
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
     
-    def legal_moves(self, current_board):
-        pass
+    def legal_moves(self):
+        legal_squares = []
+
+        # find points on diagonal pointing up (white's perspective)
+        for i in range(-8, 8):
+            point_x = self.x + i
+            point_y = self.y + i
+
+            if (point_x < 0 or point_x > 7) or (point_y < 0 or point_y > 7):
+                continue
+            elif point_x == self.x:
+                continue
+            else:
+                legal_squares.append([point_x, point_y])
+
+        # find points on diagonal pointing down (white's perspective)
+        for i in range(-8, 8):
+            point_x = self.x + i
+            point_y = self.y - i
+
+            if (point_x < 0 or point_x > 7) or (point_y < 0 or point_y > 7):
+                continue
+            elif point_x == self.x:
+                continue
+            else:
+                legal_squares.append([point_x, point_y])
+
+        return legal_squares
 
     def reset(self, x, y):
         self.x = x
--- src/pieces/queen.py
+++ src/pieces/queen.py
@@ -7,8 +7,46 @@ class Queen(pieces.Piece):
     def __init__(self, x, y, color):
         super().__init__(x, y, color)
 
-    def legal_moves(self, current_board):
-        pass
+    def legal_moves(self):
+        legal_squares = []
+
+        # moves horizontally/vertically
+        for x in range(8):
+            if x == self.x:
+                continue
+            legal_squares.append([x, self.y])
+
+        for y in range(8):
+            if y == self.y:
+                continue
+            legal_squares.append([self.x, y])
+        
+        # moves diagonally
+        # find points on diagonal pointing up (white's perspective)
+        for i in range(-8, 8):
+            point_x = self.x + i
+            point_y = self.y + i
+
+            if (point_x < 0 or point_x > 7) or (point_y < 0 or point_y > 7):
+                continue
+            elif point_x == self.x:
+                continue
+            else:
+                legal_squares.append([point_x, point_y])
+
+        # find points on diagonal pointing down (white's perspective)
+        for i in range(-8, 8):
+            point_x = self.x + i
+            point_y = self.y - i
+
+            if (point_x < 0 or point_x > 7) or (point_y < 0 or point_y > 7):
+                continue
+            elif point_x == self.x:
+                continue
+            else:
+                legal_squares.append([point_x, point_y])
+
+        return legal_squares
 
     def reset(self, x, y):
         self.x = x
