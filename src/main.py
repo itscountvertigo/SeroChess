19b84357bf76580eab40a70222a98ee807a055fa
Anbiya Popal <anbiya2004@gmail.com>
2021-07-08 13:54:18 +0200

    create objects for all pieces

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
 from pieces import king, queen, rook, knight, bishop, pawn
+
+board = []
+
+board.append(king.King(4, 0, 1)) # white's king
+board.append(queen.Queen(3, 0, 1)) # white's queen
+board.append(bishop.Bishop(2, 0, 1)) # white's dark square bishop
+board.append(bishop.Bishop(5, 0, 1)) # white's light square bishop
+board.append(knight.Knight(1, 0, 1)) # whites knight that starts on b
+board.append(knight.Knight(6, 0, 1)) # white's knight that starts on g
+board.append(rook.Rook(0, 0, 1)) # white's rook that starts on a
+board.append(rook.Rook(7, 0, 1)) # white's rook that starts on h
+
+board.append(king.King(4, 7, 0)) # black's king
+board.append(queen.Queen(3, 7, 0)) # black's queen
+board.append(bishop.Bishop(2, 7, 0)) # black's dark square bishop
+board.append(bishop.Bishop(5, 7, 0)) # black's light square bishop
+board.append(knight.Knight(1, 7, 0)) # blacks knight that starts on b
+board.append(knight.Knight(6, 7, 0)) # black's knight that starts on g
+board.append(rook.Rook(0, 7, 0)) # black's rook that starts on a
+board.append(rook.Rook(7, 7, 0)) # white's rook that starts on h
+
+print(board[0].description())
