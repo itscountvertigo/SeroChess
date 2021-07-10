45f8a268f114e053c06b656bdaac00d96d4a5904
Anbiya Popal <anbiya2004@gmail.com>
2021-07-10 15:38:44 +0200

    use a cleaner and more readable way of creating the board array

--- src/main.py
+++ src/main.py
@@ -1,25 +1,17 @@
 from pieces import king, queen, rook, knight, bishop, pawn
 
-board = []
-
-board.append(king.King(4, 0, 1)) # white's king
-board.append(queen.Queen(3, 0, 1)) # white's queen
-board.append(bishop.Bishop(2, 0, 1)) # white's dark square bishop
-board.append(bishop.Bishop(5, 0, 1)) # white's light square bishop
-board.append(knight.Knight(1, 0, 1)) # whites knight that starts on b
-board.append(knight.Knight(6, 0, 1)) # white's knight that starts on g
-board.append(rook.Rook(0, 0, 1)) # white's rook that starts on a
-board.append(rook.Rook(7, 0, 1)) # white's rook that starts on h
-
-board.append(king.King(4, 7, 0)) # black's king
-board.append(queen.Queen(3, 7, 0)) # black's queen
-board.append(bishop.Bishop(2, 7, 0)) # black's dark square bishop
-board.append(bishop.Bishop(5, 7, 0)) # black's light square bishop
-board.append(knight.Knight(1, 7, 0)) # blacks knight that starts on b
-board.append(knight.Knight(6, 7, 0)) # black's knight that starts on g
-board.append(rook.Rook(0, 7, 0)) # black's rook that starts on a
-board.append(rook.Rook(7, 7, 0)) # white's rook that starts on h
+board = [king.King(4, 0, 1),                              # white king
+         queen.Queen(3, 0, 1),                            # white queen
+         bishop.Bishop(2, 0, 1), bishop.Bishop(5, 0, 1),  # white bishops
+         knight.Knight(1, 0, 1), knight.Knight(6, 0, 1),  # white knights
+         rook.Rook(0, 0, 1), rook.Rook(7, 0, 1),          # white rooks
+         king.King(4, 7, 0),                              # black king
+         queen.Queen(3, 7, 0),                            # black queen
+         bishop.Bishop(2, 7, 0), bishop.Bishop(5, 7, 0),  # black bishops
+         knight.Knight(1, 7, 0), knight.Knight(6, 7, 0),  # black knights
+         rook.Rook(0, 7, 0), rook.Rook(7, 7, 0)           # black rooks
+         ]
 
 for i in range(8):
-    board.append(pawn.Pawn(i, 1, 1))
-    board.append(pawn.Pawn(i, 6, 0))
+    board.append(pawn.Pawn(i, 1, 1))  # add white pawns
+    board.append(pawn.Pawn(i, 6, 0))  # add black pawns
