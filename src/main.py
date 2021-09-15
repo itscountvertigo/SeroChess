30c740973199201241687e29040ea9d0e05d5dd4
Anbiya Popal <anbiya2004@gmail.com>
2021-09-15 14:39:13 +0200

    make gui.py and main.py the same file and add checkerboard

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import arcade
+
 from pieces import king, queen, rook, knight, bishop, pawn
 
 board = [king.King(4, 0, 1),                              # white king
@@ -10,8 +12,45 @@ board = [king.King(4, 0, 1),                              # white king
          bishop.Bishop(2, 7, 0), bishop.Bishop(5, 7, 0),  # black bishops
          knight.Knight(1, 7, 0), knight.Knight(6, 7, 0),  # black knights
          rook.Rook(0, 7, 0), rook.Rook(7, 7, 0)           # black rooks
-         ]
+        ]
 
 for i in range(8):
     board.append(pawn.Pawn(i, 1, 1))  # add white pawns
     board.append(pawn.Pawn(i, 6, 0))  # add black pawns
+
+class GUI(arcade.Window):
+    """
+    The main class for the GUI. This class inherits from the arcade library.
+    This is used to display the board.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        # i stole lichess's palette
+        self.color_w = [240, 217, 181]
+        self.color_b = [181, 136, 99]
+        
+        arcade.set_background_color(self.color_w)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.draw_squares(self.width / 8, self.height / 8)
+
+    def draw_squares(self, square_width, square_height):
+        for x in range(8):
+            for y in range(8):
+                if x % 2 != y % 2:
+                    arcade.draw_rectangle_filled(x * square_width + square_width / 2, 
+                                                 y * square_height + square_height / 2, 
+                                                 square_width, 
+                                                 square_height, 
+                                                 self.color_b)
+
+def main():
+    window = GUI(800, 800, "PWS Chess Engine")
+    # window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
