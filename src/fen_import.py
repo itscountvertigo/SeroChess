d446ccafe8c03d05ff7a962238efb2250a5115d8
unknown <213302@unic-utrecht.nl>
2021-11-05 11:52:49 +0100

    fixt revers issue

--- src/fen_import.py
+++ src/fen_import.py
@@ -4,14 +4,14 @@ def read_FEN(FEN_code):
     output_FEN=[]
     FENlist=list(FEN_code)
     X=0
-    Y=0
+    Y=7
     #println(FENlist)
     for i in range(len(FENlist)):
-        if Y<8 and X<9:
+        if Y>-1 and X<9:
             if FENlist[i]=='/':
                 #print("hoi")
                 X=0
-                Y += 1
+                Y -= 1
                 #println(Y)
                 #black pieces
             elif FENlist[i]=='r':
@@ -53,7 +53,7 @@ def read_FEN(FEN_code):
                 output_FEN.append(pawn.Pawn(X, Y, 1))
                 X+=1
             elif FENlist[i]==' ':
-                Y+=1
+                Y-=1
             else:
                 for j in range(48,57):
                     if FENlist[i]==chr(j):
