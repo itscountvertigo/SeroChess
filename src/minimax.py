13005be2bfded04812ad6795f7799f04c60d6652
Anbiya Popal <anbiya2004@gmail.com>
2021-12-14 16:16:45 +0100

    fix issue with opening handling causing it to make opening moves when it shouldn't

--- src/minimax.py
+++ src/minimax.py
@@ -8,26 +8,31 @@ from parse_openings import parse_openings
 openings = parse_openings()
 
 def minimax(current_board, who_to_move, alpha, beta, depth):
-
     if all_legal_moves(current_board, who_to_move) == []: 
         return (0, None)
     
     if depth == 0:
         return (evaluate.evaluate(current_board), None)
 
+    global openings
+
     if openings != []:
-        # Removes irrelevant openings (of positions that have not occured in this game)
-        for idx, move in enumerate(current_board.moves):
-            for idx2, opening in enumerate(openings):
-                if len(opening) <= current_board.ply:
-                    openings.pop(idx2)
-                    continue
-                if opening[idx] != move:
-                    openings.pop(idx2)
+        new_openings = []
+        for opening in openings:
+            if len(opening) <= current_board.ply:
+                continue
+            for ply, move in enumerate(current_board.moves):
+                if opening[ply] != move:
+                    break
+                new_openings.append(opening)
+        
+        openings = new_openings 
 
     # if there are still relevant openings, play one of the moves
-    if openings != []: # This is checked again because the openings array is changed above
-        return (None, openings[random.randint(0, len(openings)) - 1][current_board.ply])
+    if openings != []:
+        random_opening = openings[random.randint(0, len(openings) - 1)]
+        print(f"move from opening: {random_opening}")
+        return (None, random_opening[current_board.ply])
 
     if who_to_move == 1:
         evaluation = (-999, None)
@@ -43,6 +48,15 @@ def minimax(current_board, who_to_move, alpha, beta, depth):
                 break
 
             alpha = alpha if alpha[0] >= evaluation[0] else evaluation
+
+        has_king_w = False
+
+        for piece in current_board.pieces:
+            if piece.__class__.__name__ == 'King':
+                has_king_w = True
+        
+        if not has_king_w:
+            return (-9999, "Black wins")
 
         return evaluation
 
@@ -62,5 +76,14 @@ def minimax(current_board, who_to_move, alpha, beta, depth):
                 break
 
             beta = beta if beta[0] <= evaluation[0] else evaluation
+
+        has_king_b = False
+
+        for piece in current_board.pieces:
+            if piece.__class__.__name__ == 'King':
+                has_king_b = True
+        
+        if not has_king_b:
+            return (9999, "White wins")
 
         return evaluation
