5a2d682116ac0764fc925275c37a16029a9bc9d9
Anbiya Popal <anbiya2004@gmail.com>
2021-12-17 21:19:28 +0100

    remove commented code from final product

--- src/board.py
+++ src/board.py
@@ -12,39 +12,6 @@ class Board():
         self.ply = ply
     
     def move(self, move):
-        """
-        if move == "SHORT_CASTLE":
-            for piece in self.pieces:
-                if piece.__class__.__name__ == "King" and piece.color == self.who_to_move:
-                    piece.x = 6
-
-                if piece.__class__.__name__ == "Rook" and piece.color == self.who_to_move and piece.x == 7:
-                    piece.x = 5
-
-            if piece.color == 0:
-                self.move_num += 1
-            self.ply += 1
-
-            return
-
-        if move == "LONG_CASTLE":
-            for piece in self.pieces:
-                if piece.__class__.__name__ == "King" and piece.color == self.who_to_move:
-                    piece.x = 2
-
-                print(piece.__class__.__name__, piece.color, piece.x)
-
-                if piece.__class__.__name__ == "Rook" and piece.color == self.who_to_move and piece.x == 0:
-                    print("here")
-                    piece.x = 3
-
-            if piece.color == 0:
-                self.move_num += 1
-            self.ply += 1
-
-            return
-        """
-
         coords = move_coords.move_to_coords(move)
         old_coords = coords[0]
         new_coords = coords[1]
@@ -105,3 +72,4 @@ class Board():
         return 0
 
 main_board = Board(read_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"), 1, 1, 0, [])
+# main_board = Board(read_FEN("7k/7r/6r1/K7/8/8/8/8 b KQkq - 0 1"), 0, 1, 0, []) # mate in 2 puzzle
