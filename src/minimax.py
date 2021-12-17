f138b528d33ead0b463cba3d749a6dce2cbbbc9e
Anbiya Popal <anbiya2004@gmail.com>
2021-12-17 11:06:18 +0100

    write cleaner code for previous fix

--- src/minimax.py
+++ src/minimax.py
@@ -15,16 +15,15 @@ def minimax(current_board, who_to_move, alpha, beta, depth):
 
     global openings
 
-    if openings != []:
+    if openings != [] and current_board.moves != []:
         new_openings = []
-        if current_board.moves != []:
-            for opening in openings:
-                if len(opening) <= current_board.ply:
-                    continue
-                for ply, move in enumerate(current_board.moves):
-                    if opening[ply] != move:
-                        break
-                    new_openings.append(opening)
+        for opening in openings:
+            if len(opening) <= current_board.ply:
+                continue
+            for ply, move in enumerate(current_board.moves):
+                if opening[ply] != move:
+                    break
+                new_openings.append(opening)
 
             openings = new_openings 
 
